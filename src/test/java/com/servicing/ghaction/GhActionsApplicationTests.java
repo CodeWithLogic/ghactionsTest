52d7f11c7dbe94d0a484e69ba66d917861787684
CodeWithLogic <alamp881@gmail.com>
2025-08-19 15:56:12 +0530

    initial commit

---
+++ src/test/java/com/servicing/ghaction/GhActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.servicing.ghaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GhActionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
